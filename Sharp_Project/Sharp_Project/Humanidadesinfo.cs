5b985c2b03bc6e54f3cfe4cc145c1d75ea7665c4
Fenx <zagj080422hgtvrsa8@cbtis217.edu.mx>
2024-10-31 08:35:58 -0600

    Se creo la rama Jesus22 y se copiaron lo cambios de la rama Jesus ahi, tambien se borro la rama Jesus22

---
+++ Sharp_Project/Sharp_Project/Humanidadesinfo.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: fenx2
+ * Date: 31/10/2024
+ * Time: 08:28 a. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Sharp_Project
+{
+	/// <summary>
+	/// Description of Humanidadesinfo.
+	/// </summary>
+	public partial class Humanidadesinfo : Form
+	{
+		public Humanidadesinfo()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
