be759e517d82b479e586df59c2896e792d4f8ca6
s0ymigue <pemm080223hdfrnga2@cbtis217.edu.mx>
2024-10-31 08:00:55 -0600

    Matematicas sin moda...

--- Sharp_Project/Sharp_Project/MatematicasForm.Designer.cs
+++ Sharp_Project/Sharp_Project/MatematicasForm.Designer.cs
@@ -36,12 +36,149 @@ namespace Sharp_Project
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.lblPromedio = new System.Windows.Forms.Label();
+			this.lblReprobados = new System.Windows.Forms.Label();
+			this.lblAprobados = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.lblMenor = new System.Windows.Forms.Label();
+			this.lblMayor = new System.Windows.Forms.Label();
+			this.lblPromAprobados = new System.Windows.Forms.Label();
+			this.lblMedia = new System.Windows.Forms.Label();
+			this.lblModa = new System.Windows.Forms.Label();
+			this.txtCalificaciones = new System.Windows.Forms.TextBox();
+			this.btnCalcular = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// lblPromedio
+			// 
+			this.lblPromedio.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblPromedio.Location = new System.Drawing.Point(12, 162);
+			this.lblPromedio.Name = "lblPromedio";
+			this.lblPromedio.Size = new System.Drawing.Size(254, 46);
+			this.lblPromedio.TabIndex = 0;
+			this.lblPromedio.Text = "$$$$$$";
+			// 
+			// lblReprobados
+			// 
+			this.lblReprobados.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblReprobados.Location = new System.Drawing.Point(272, 162);
+			this.lblReprobados.Name = "lblReprobados";
+			this.lblReprobados.Size = new System.Drawing.Size(254, 46);
+			this.lblReprobados.TabIndex = 1;
+			this.lblReprobados.Text = "$$$$$$";
+			// 
+			// lblAprobados
+			// 
+			this.lblAprobados.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblAprobados.Location = new System.Drawing.Point(532, 162);
+			this.lblAprobados.Name = "lblAprobados";
+			this.lblAprobados.Size = new System.Drawing.Size(254, 46);
+			this.lblAprobados.TabIndex = 2;
+			this.lblAprobados.Text = "$$$$$$";
+			// 
+			// label4
+			// 
+			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label4.Location = new System.Drawing.Point(532, 208);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(254, 46);
+			this.label4.TabIndex = 5;
+			this.label4.Text = "$$$$$$";
+			// 
+			// lblMenor
+			// 
+			this.lblMenor.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblMenor.Location = new System.Drawing.Point(272, 208);
+			this.lblMenor.Name = "lblMenor";
+			this.lblMenor.Size = new System.Drawing.Size(254, 46);
+			this.lblMenor.TabIndex = 4;
+			this.lblMenor.Text = "$$$$$$";
+			// 
+			// lblMayor
+			// 
+			this.lblMayor.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblMayor.Location = new System.Drawing.Point(12, 208);
+			this.lblMayor.Name = "lblMayor";
+			this.lblMayor.Size = new System.Drawing.Size(254, 46);
+			this.lblMayor.TabIndex = 3;
+			this.lblMayor.Text = "$$$$$$";
+			// 
+			// lblPromAprobados
+			// 
+			this.lblPromAprobados.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblPromAprobados.Location = new System.Drawing.Point(532, 254);
+			this.lblPromAprobados.Name = "lblPromAprobados";
+			this.lblPromAprobados.Size = new System.Drawing.Size(254, 46);
+			this.lblPromAprobados.TabIndex = 8;
+			this.lblPromAprobados.Text = "$$$$$$";
+			// 
+			// lblMedia
+			// 
+			this.lblMedia.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblMedia.Location = new System.Drawing.Point(272, 254);
+			this.lblMedia.Name = "lblMedia";
+			this.lblMedia.Size = new System.Drawing.Size(254, 46);
+			this.lblMedia.TabIndex = 7;
+			this.lblMedia.Text = "$$$$$$";
+			// 
+			// lblModa
+			// 
+			this.lblModa.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblModa.Location = new System.Drawing.Point(12, 254);
+			this.lblModa.Name = "lblModa";
+			this.lblModa.Size = new System.Drawing.Size(254, 46);
+			this.lblModa.TabIndex = 6;
+			this.lblModa.Text = "$$$$$$";
+			// 
+			// txtCalificaciones
+			// 
+			this.txtCalificaciones.Location = new System.Drawing.Point(12, 12);
+			this.txtCalificaciones.Multiline = true;
+			this.txtCalificaciones.Name = "txtCalificaciones";
+			this.txtCalificaciones.Size = new System.Drawing.Size(818, 78);
+			this.txtCalificaciones.TabIndex = 9;
+			// 
+			// btnCalcular
+			// 
+			this.btnCalcular.Location = new System.Drawing.Point(287, 373);
+			this.btnCalcular.Name = "btnCalcular";
+			this.btnCalcular.Size = new System.Drawing.Size(300, 89);
+			this.btnCalcular.TabIndex = 10;
+			this.btnCalcular.Text = "Calcular";
+			this.btnCalcular.UseVisualStyleBackColor = true;
+			this.btnCalcular.Click += new System.EventHandler(this.BtnCalcularClick);
 			// 
 			// MatematicasForm
 			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Text = "MatematicasForm";
+			this.ClientSize = new System.Drawing.Size(842, 474);
+			this.Controls.Add(this.btnCalcular);
+			this.Controls.Add(this.txtCalificaciones);
+			this.Controls.Add(this.lblPromAprobados);
+			this.Controls.Add(this.lblMedia);
+			this.Controls.Add(this.lblModa);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.lblMenor);
+			this.Controls.Add(this.lblMayor);
+			this.Controls.Add(this.lblAprobados);
+			this.Controls.Add(this.lblReprobados);
+			this.Controls.Add(this.lblPromedio);
 			this.Name = "MatematicasForm";
+			this.Text = "MatematicasForm";
+			this.ResumeLayout(false);
+			this.PerformLayout();
 		}
+		private System.Windows.Forms.Button btnCalcular;
+		private System.Windows.Forms.TextBox txtCalificaciones;
+		private System.Windows.Forms.Label lblModa;
+		private System.Windows.Forms.Label lblMedia;
+		private System.Windows.Forms.Label lblPromAprobados;
+		private System.Windows.Forms.Label lblMayor;
+		private System.Windows.Forms.Label lblMenor;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label lblAprobados;
+		private System.Windows.Forms.Label lblReprobados;
+		private System.Windows.Forms.Label lblPromedio;
 	}
 }
--- Sharp_Project/Sharp_Project/MatematicasForm.cs
+++ Sharp_Project/Sharp_Project/MatematicasForm.cs
@@ -8,6 +8,7 @@
  */
 using System;
 using System.Drawing;
+using System.Collections.Generic;
 using System.Windows.Forms;
 
 namespace Sharp_Project
@@ -27,6 +28,64 @@ namespace Sharp_Project
 			//
 			// TODO: Add constructor code after the InitializeComponent() call.
 			//
+		}
+		
+		void BtnCalcularClick(object sender, EventArgs e)
+		{
+			// Tomar todas las calificaciones del TextBox
+            string input = txtCalificaciones.Text;
+
+            // Separar las calificaciones por comas, espacios o saltos de línea
+            string[] califArray = input.Split(new char[] { ',', ' ', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+
+            // Convertir el array en una lista de enteros
+            List<int> calificaciones = new List<int>();
+            foreach (string califStr in califArray)
+            {
+                int calif;
+                if (int.TryParse(califStr, out calif))
+                {
+                    calificaciones.Add(calif);
+                }
+            }
+
+            // Validar que haya exactamente 50 calificaciones
+            if (calificaciones.Count == 50)
+            {
+                // Calcular estadísticas básicas
+                int suma = 0, aprobados = 0, reprobados = 0;
+                int mayor = calificaciones[0];
+                int menor = calificaciones[0];
+
+                foreach (int calificacion in calificaciones)
+                {
+                    suma += calificacion;
+                    if (calificacion >= 60)
+                        aprobados++;
+                    else
+                        reprobados++;
+
+                    if (calificacion > mayor) mayor = calificacion;
+                    if (calificacion < menor) menor = calificacion;
+                }
+
+                // Calcular el promedio
+                double promedio = (double)suma / calificaciones.Count;
+
+                // Actualizar los labels con los resultados
+                lblPromedio.Text = "Promedio: " + promedio;
+                lblReprobados.Text = "Reprobados: " + reprobados;
+                lblAprobados.Text = "Aprobados: " + aprobados;
+                lblMayor.Text = "Mayor: " + mayor;
+                lblMenor.Text = "Menor: " + menor;
+                lblPromAprobados = "PromAprobados: " +
+            }
+            else
+            {
+                MessageBox.Show("Por favor, ingresa exactamente 50 calificaciones válidas.");
+            }
+            
+            
 		}
 	}
 }
