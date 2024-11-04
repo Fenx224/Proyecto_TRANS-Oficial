e756facb06f0056e8712cfe160e32d27060c3b48
Fenx <zagj080422hgtvrsa8@cbtis217.edu.mx>
2024-11-04 07:54:43 -0600

    Se agrego contenido a la segunda ventana de humanidades

--- Sharp_Project/Sharp_Project/HumanidadesForm.Designer.cs
+++ Sharp_Project/Sharp_Project/HumanidadesForm.Designer.cs
@@ -38,46 +38,55 @@ namespace Sharp_Project
 		{
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HumanidadesForm));
 			this.label1 = new System.Windows.Forms.Label();
-			this.button1 = new System.Windows.Forms.Button();
 			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
 			this.SuspendLayout();
 			// 
 			// label1
 			// 
-			this.label1.Location = new System.Drawing.Point(175, 9);
+			this.label1.Location = new System.Drawing.Point(367, 9);
 			this.label1.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
 			this.label1.Name = "label1";
 			this.label1.Size = new System.Drawing.Size(239, 39);
 			this.label1.TabIndex = 0;
 			this.label1.Text = "Humanidades";
 			// 
-			// button1
-			// 
-			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.button1.Location = new System.Drawing.Point(523, 438);
-			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(75, 23);
-			this.button1.TabIndex = 1;
-			this.button1.Text = "Entrar";
-			this.button1.UseVisualStyleBackColor = true;
-			this.button1.Click += new System.EventHandler(this.Button1Click);
-			// 
 			// label2
 			// 
-			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.label2.Location = new System.Drawing.Point(31, 78);
 			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(551, 93);
+			this.label2.Size = new System.Drawing.Size(149, 34);
 			this.label2.TabIndex = 2;
-			this.label2.Text = resources.GetString("label2.Text");
+			this.label2.Text = "Etica y moral";
+			// 
+			// label3
+			// 
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Location = new System.Drawing.Point(31, 109);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(900, 70);
+			this.label3.TabIndex = 3;
+			this.label3.Text = resources.GetString("label3.Text");
+			// 
+			// label4
+			// 
+			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label4.Location = new System.Drawing.Point(31, 176);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(900, 83);
+			this.label4.TabIndex = 4;
+			this.label4.Text = resources.GetString("label4.Text");
 			// 
 			// HumanidadesForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(18F, 36F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(610, 465);
+			this.ClientSize = new System.Drawing.Size(975, 563);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
 			this.Controls.Add(this.label2);
-			this.Controls.Add(this.button1);
 			this.Controls.Add(this.label1);
 			this.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.Margin = new System.Windows.Forms.Padding(7);
@@ -85,8 +94,9 @@ namespace Sharp_Project
 			this.Text = "HumanidadesForm";
 			this.ResumeLayout(false);
 		}
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label3;
 		private System.Windows.Forms.Label label2;
-		private System.Windows.Forms.Button button1;
 		private System.Windows.Forms.Label label1;
 	}
 }
--- Sharp_Project/Sharp_Project/Humanidadesinfo.Designer.cs
+++ Sharp_Project/Sharp_Project/Humanidadesinfo.Designer.cs
@@ -36,13 +36,112 @@ namespace Sharp_Project
 		/// </summary>
 		private void InitializeComponent()
 		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Humanidadesinfo));
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(276, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(241, 39);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "HUMANIDADES";
+			// 
+			// label2
+			// 
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(327, 48);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(138, 23);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "El bien y el mal";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(674, 387);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 2;
+			this.button1.Text = "Entrar";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// label3
+			// 
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Location = new System.Drawing.Point(72, 107);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(631, 61);
+			this.label3.TabIndex = 3;
+			this.label3.Text = "Aprende a como tener un criterio objetivo e individual sobre los temas de relevan" +
+			"cia para tu persona y aprende a debatir desde una postura objetiva, madura e imp" +
+			"arcial.";
+			this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// label4
+			// 
+			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label4.Location = new System.Drawing.Point(72, 202);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(415, 104);
+			this.label4.TabIndex = 4;
+			this.label4.Text = resources.GetString("label4.Text");
+			// 
+			// label5
+			// 
+			this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label5.Location = new System.Drawing.Point(72, 325);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(653, 69);
+			this.label5.TabIndex = 5;
+			this.label5.Text = "Aparte de debatir, tambien se centra en tratar de contestar interrogantes como, ¿" +
+			"Que es el bien y que es el mal?, o ¿El se humano es malo por naturaleza o lo vue" +
+			"lven malo?";
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.ErrorImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.ErrorImage")));
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(515, 152);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(188, 173);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox1.TabIndex = 6;
+			this.pictureBox1.TabStop = false;
 			// 
 			// Humanidadesinfo
 			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Text = "Humanidadesinfo";
+			this.ClientSize = new System.Drawing.Size(761, 422);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
 			this.Name = "Humanidadesinfo";
+			this.Text = "Humanidadesinfo";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
 		}
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
 		
 		public void Show()
 		{
