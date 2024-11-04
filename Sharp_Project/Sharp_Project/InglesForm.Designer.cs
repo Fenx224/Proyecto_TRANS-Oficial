99148b4e6eec47344071ca18522f740aa2c71e2f
MuzanChiquito <cris.morrizz@gmail.com>
2024-11-04 08:39:12 -0600

    Ingles

--- Sharp_Project/Sharp_Project/InglesForm.Designer.cs
+++ Sharp_Project/Sharp_Project/InglesForm.Designer.cs
@@ -14,6 +14,13 @@ namespace Sharp_Project
 		/// Designer variable used to keep track of non-visual components.
 		/// </summary>
 		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.PictureBox pictureBox2;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label5;
 		
 		/// <summary>
 		/// Disposes resources used by the form.
@@ -36,12 +43,101 @@ namespace Sharp_Project
 		/// </summary>
 		private void InitializeComponent()
 		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(InglesForm));
+			this.label1 = new System.Windows.Forms.Label();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.pictureBox2 = new System.Windows.Forms.PictureBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("Modern No. 20", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(675, 43);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(192, 72);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Ingles";
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(173, 12);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(312, 166);
+			this.pictureBox1.TabIndex = 1;
+			this.pictureBox1.TabStop = false;
+			// 
+			// pictureBox2
+			// 
+			this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+			this.pictureBox2.Location = new System.Drawing.Point(1061, 7);
+			this.pictureBox2.Name = "pictureBox2";
+			this.pictureBox2.Size = new System.Drawing.Size(300, 171);
+			this.pictureBox2.TabIndex = 2;
+			this.pictureBox2.TabStop = false;
+			// 
+			// label2
+			// 
+			this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(193, 181);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(271, 129);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "Precente, Pasado\r\n y \r\nFuturo";
+			this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+			// 
+			// label3
+			// 
+			this.label3.Font = new System.Drawing.Font("Microsoft YaHei UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Location = new System.Drawing.Point(83, 294);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(536, 752);
+			this.label3.TabIndex = 4;
+			this.label3.Text = resources.GetString("label3.Text");
+			this.label3.Click += new System.EventHandler(this.Label3Click);
+			// 
+			// label4
+			// 
+			this.label4.Font = new System.Drawing.Font("Microsoft YaHei UI", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label4.Location = new System.Drawing.Point(1009, 210);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(382, 100);
+			this.label4.TabIndex = 5;
+			this.label4.Text = "Present Continuous Tense With\r\nFuture Intention\r\n";
+			this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+			// 
+			// label5
+			// 
+			this.label5.Location = new System.Drawing.Point(946, 320);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(513, 576);
+			this.label5.TabIndex = 6;
+			this.label5.Text = resources.GetString("label5.Text");
 			// 
 			// InglesForm
 			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Text = "InglesForm";
+			this.ClientSize = new System.Drawing.Size(1471, 1055);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.pictureBox2);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.label1);
 			this.Name = "InglesForm";
+			this.Text = "InglesForm";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+			this.ResumeLayout(false);
+
 		}
 	}
 }
