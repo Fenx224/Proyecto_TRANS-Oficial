b54341e375bc0ba7eb05d31ea7b18e7346d75291
Fenx <zagj080422hgtvrsa8@cbtis217.edu.mx>
2024-11-04 08:34:43 -0600

    Se termino la materia de humanidades

--- Sharp_Project/Sharp_Project/HumanidadesForm.Designer.cs
+++ Sharp_Project/Sharp_Project/HumanidadesForm.Designer.cs
@@ -41,6 +41,11 @@ namespace Sharp_Project
 			this.label2 = new System.Windows.Forms.Label();
 			this.label3 = new System.Windows.Forms.Label();
 			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
+			this.label8 = new System.Windows.Forms.Label();
+			this.label9 = new System.Windows.Forms.Label();
 			this.SuspendLayout();
 			// 
 			// label1
@@ -79,11 +84,62 @@ namespace Sharp_Project
 			this.label4.TabIndex = 4;
 			this.label4.Text = resources.GetString("label4.Text");
 			// 
+			// label5
+			// 
+			this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label5.Location = new System.Drawing.Point(31, 288);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(210, 29);
+			this.label5.TabIndex = 5;
+			this.label5.Text = "Objetivo y subjetivo";
+			// 
+			// label6
+			// 
+			this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label6.Location = new System.Drawing.Point(31, 321);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(900, 153);
+			this.label6.TabIndex = 6;
+			this.label6.Text = resources.GetString("label6.Text");
+			// 
+			// label7
+			// 
+			this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label7.Location = new System.Drawing.Point(31, 478);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(323, 28);
+			this.label7.TabIndex = 7;
+			this.label7.Text = "La vida despues de la muerte";
+			// 
+			// label8
+			// 
+			this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label8.Location = new System.Drawing.Point(31, 510);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(900, 106);
+			this.label8.TabIndex = 8;
+			this.label8.Text = resources.GetString("label8.Text");
+			// 
+			// label9
+			// 
+			this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label9.Location = new System.Drawing.Point(141, 638);
+			this.label9.Name = "label9";
+			this.label9.Size = new System.Drawing.Size(708, 114);
+			this.label9.TabIndex = 9;
+			this.label9.Text = resources.GetString("label9.Text");
+			this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
 			// HumanidadesForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(18F, 36F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(975, 563);
+			this.ClientSize = new System.Drawing.Size(975, 786);
+			this.Controls.Add(this.label9);
+			this.Controls.Add(this.label8);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label5);
 			this.Controls.Add(this.label4);
 			this.Controls.Add(this.label3);
 			this.Controls.Add(this.label2);
@@ -94,6 +150,11 @@ namespace Sharp_Project
 			this.Text = "HumanidadesForm";
 			this.ResumeLayout(false);
 		}
+		private System.Windows.Forms.Label label9;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label label5;
 		private System.Windows.Forms.Label label4;
 		private System.Windows.Forms.Label label3;
 		private System.Windows.Forms.Label label2;
